34c86c7fb8d441d5ae10fdfbf74bf811eb4244a0
Joe Webster <joseph.scott.webster@gmail.com>
2024-04-22 16:40:31 -0400

    Add edge function comments and improved error handling

--- supabase/functions/process-user-speech-text/index.ts
+++ supabase/functions/process-user-speech-text/index.ts
@@ -5,27 +5,39 @@ import OpenAI from "https://deno.land/x/openai@v4.32.1/mod.ts";
 const openai = new OpenAI({ apiKey: `${Deno.env.get("OPENAI_API_KEY")}` });
 
 Deno.serve(async (req) => {
-  const { speechText, messages } = await req.json();
-
   try {
-    const userMessage = {
+    // Parse JSON body from the request
+    const { speechText, messages }: {
+      speechText: string;
+      messages: OpenAI.ChatCompletionMessageParam[];
+    } = await req.json();
+
+    // Construct the user message
+    const userMessage: OpenAI.ChatCompletionMessageParam = {
       role: "user",
       content: speechText,
     };
 
+    // Generate AI completion based on the conversation history
     const completion = await openai.chat.completions.create({
       messages: [...messages, userMessage],
       model: "gpt-3.5-turbo",
     });
 
-    const assistantMessage = completion.choices[0].message;
+    // Extract the assistant's message from the completion
+    const assistantMessage = {
+      role: "assistant",
+      content: completion.choices[0].message.content,
+    };
 
+    // Generate speech from the assistant's message
     const mp3Response = await openai.audio.speech.create({
       model: "tts-1",
       voice: "nova",
       input: assistantMessage.content!,
     });
 
+    // Encode the MP3 data to base64
     const mp3ArrayBuffer = await mp3Response.arrayBuffer();
     const encodedMp3Data = encodeBase64(mp3ArrayBuffer);
 
@@ -36,11 +48,13 @@ Deno.serve(async (req) => {
       { headers: { "Content-Type": "application/json" } },
     );
   } catch (error) {
-    console.log(error);
-  }
+    console.error("Error handling request:", error);
 
-  return new Response(
-    JSON.stringify(speechText),
-    { headers: { "Content-Type": "application/json" } },
-  );
+    return new Response(
+      JSON.stringify({
+        error: "An error occurred while processing your request.",
+      }),
+      { status: 500, headers: { "Content-Type": "application/json" } },
+    );
+  }
 });
