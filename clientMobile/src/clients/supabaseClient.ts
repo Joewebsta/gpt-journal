e0a93e2694e30321834a9002083c7f0a8b0075f0
Joe Webster <joseph.scott.webster@gmail.com>
2024-05-14 10:03:35 -0400

    Remove unecessary comment

--- clientMobile/src/clients/supabaseClient.ts
+++ clientMobile/src/clients/supabaseClient.ts
@@ -9,7 +9,7 @@ import { createClient } from "@supabase/supabase-js";
 // PRODUCTION;
 const supabase = createClient(
   process.env.SUPABASE_URL || "",
-  process.env.SUPABASE_ANON_KEY || "", // Provide a default value for SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY || "",
 );
 
 export default supabase;
