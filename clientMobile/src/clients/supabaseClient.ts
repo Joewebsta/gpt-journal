b73d311b0b090ff1fe272fcafe37c65200aaf0c9
Joe Webster <joseph.scott.webster@gmail.com>
2024-05-14 08:53:02 -0400

    Update supabase client to use production edge function

--- clientMobile/src/clients/supabaseClient.ts
+++ clientMobile/src/clients/supabaseClient.ts
@@ -1,15 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
 // LOCAL
-const supabase = createClient(
-  "http://127.0.0.1:54321",
-  process.env.SUPABASE_ANON_KEY || "",
-);
+// const supabase = createClient(
+//   "http://127.0.0.1:54321",
+//   process.env.SUPABASE_ANON_KEY || "",
+// );
 
 // PRODUCTION;
-// const supabase = createClient(
-//   process.env.SUPABASE_URL,
-//   process.env.SUPABASE_ANON_KEY
-// );
+const supabase = createClient(
+  process.env.SUPABASE_URL || "",
+  process.env.SUPABASE_ANON_KEY || "", // Provide a default value for SUPABASE_ANON_KEY
+);
 
 export default supabase;
